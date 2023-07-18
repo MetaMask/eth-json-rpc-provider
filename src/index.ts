ca9c9f44ade53a210abbbc61d37bf886c6544afe
Elliot Winkler <elliot.winkler@gmail.com>
2023-07-18 14:16:52 -0600

    Expose JsonRpcProvider

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
+export * from './json-rpc-provider';
 export * from './provider-from-engine';
 export * from './provider-from-middleware';
-export type {
-  ISafeEventEmitterProvider,
-  SafeEventEmitterProvider,
-} from './safe-event-emitter-provider';
